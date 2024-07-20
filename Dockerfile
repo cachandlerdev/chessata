2ce7fc9f2f50efeaaffdbeb75c2124a5a878628f
cachandlerdev <70250943+cachandlerdev@users.noreply.github.com>
2024-07-20 15:52:38 -0700

    feat: remove docker because that was a pain (we'll bring it back later)

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3
-COPY . /usr/src/app
-WORKDIR /usr/src/app/chess_core
-RUN pip install -r ../requirements.txt
-CMD ["python3", "chess_main.py"]
--- tests/experiments/chess_core/test_gm_chess.py
+++ tests/experiments/chess_core/test_gm_chess.py
@@ -1,4 +1,11 @@
-from chess_core.gm_chess import ChessGameMode
+from src.experiments.chess_core.game_mode.gm_chess import ChessGameMode
+
 
 def test_is_not_three():
-    
+    mode = ChessGameMode()
+    assert mode.is_three(4) == False
+
+
+def test_is_three():
+    mode = ChessGameMode()
+    assert mode.is_three(3) == True
