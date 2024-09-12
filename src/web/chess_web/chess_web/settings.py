16242aa467f2b534dd1c31f32a40df0285795475
BagelSeasoning234 <70250943+cachandlerdev@users.noreply.github.com>
2024-09-12 16:56:53 -0700

    feat: add django render tweaks

--- src/web/chess_web/chess_web/settings.py
+++ src/web/chess_web/chess_web/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import sys
+import dj_database_url
 
 from pathlib import Path
 
@@ -80,10 +81,13 @@ WSGI_APPLICATION = 'chess_web.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',        
+        conn_max_age=600)
 }
 
 
